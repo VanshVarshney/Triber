c7282b743356adc366652ebc4321b98c796858e2
Vansh Varshney <vsq.asap@gmail.com>
2020-10-05 16:21:04 +0530

    Backend Ready

--- triber-functions/dbschema.js
+++ triber-functions/dbschema.js
@@ -1,4 +1,17 @@
 let db = {
+  users: [
+    {
+      userId: 'xxxxxxxxxxxxxxxxxxxxx',
+      email: 'user@email.com',
+      handle: 'user',
+      createdAt: '2020-10-02T13:36:06.750Z',
+      imageUrl: 'image/xxxxxxxx/xxxxxxxx',
+      bio: 'Hello, My Name Is USER NAME, I am a SOftware Develope',
+      website: 'https://user.com',
+      location: 'London, UK',
+    },
+  ],
+
   screams: [
     {
       userHandle: 'user',
@@ -6,6 +19,48 @@ let db = {
       createdAt: '2020-10-02T13:36:06.750Z',
       likeCount: 5,
       commentCount: 2,
+    },
+  ],
+  comments: [
+    {
+      userHandle: 'user',
+      screamId: 'xxxxxxxxxxxxxxxxxxxxxxx',
+      body: 'nice one mate!',
+      createdAt: '2019-03-15T10:59:52.798Z',
+    },
+  ],
+  notifications: [
+    {
+      recipient: 'user',
+      sender: 'john',
+      read: 'true | false',
+      screamId: 'xxxxxxxxxxxxxxxxxxxxxxxx',
+      type: 'like | comment',
+      createdAt: '2019-03-15T10:59:52.798Z',
+    },
+  ],
+};
+
+const userDetails = {
+  // Redux data
+  credentials: {
+    userId: 'N43KJ5H43KJHREW4J5H3JWMERHB',
+    email: 'user@email.com',
+    handle: 'user',
+    createdAt: '2019-03-15T10:59:52.798Z',
+    imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+    bio: 'Hello, my name is user, nice to meet you',
+    website: 'https://user.com',
+    location: 'Lonodn, UK',
+  },
+  likes: [
+    {
+      userHandle: 'user',
+      screamId: 'hh7O5oWfWucVzGbHH2pa',
+    },
+    {
+      userHandle: 'user',
+      screamId: '3IOnFoQexRcofs5OhBXO',
     },
   ],
 };
--- triber-functions/functions/index.js
+++ triber-functions/functions/index.js
@@ -1,181 +1,170 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+
 const app = require('express')();
 
-admin.initializeApp();
+const FBAuth = require('./util/fbAuth');
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyB6NdzMYZPq9_lhuOktxgl80iFYROLVli4',
-  authDomain: 'triber-bf4d1.firebaseapp.com',
-  databaseURL: 'https://triber-bf4d1.firebaseio.com',
-  projectId: 'triber-bf4d1',
-  storageBucket: 'triber-bf4d1.appspot.com',
-  messagingSenderId: '818206872379',
-  appId: '1:818206872379:web:e8fc87b792f3e8defedd1e',
-  measurementId: 'G-WCNZZ5RZ14',
-};
+const cors = require('cors');
+app.use(cors());
 
-const firebase = require('firebase');
-const { onRequest } = require('firebase-functions/lib/providers/https');
-firebase.initializeApp(firebaseConfig);
+const { db } = require('./util/admin');
 
-const db = admin.firestore();
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream,
+  likeScream,
+  unlikeScream,
+  deleteScream,
+} = require('./handlers/screams');
 
-// Get Screams
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser,
+} = require('./handlers/users');
 
-app.get('/screams', (req, res) => {
-  db.collection('screams')
-    .orderBy('createdAt', 'desc')
-    .get()
-    .then((data) => {
-      let screams = [];
-      data.forEach((doc) => {
-        screams.push({
-          screamId: doc.id,
-          body: doc.data().body,
-          userHandle: doc.data().userHandle,
-          createdAt: doc.data().createdAt,
-        });
+// Scream Route
+
+app.get('/screams', getAllScreams);
+app.post('/scream', FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
+app.delete('/scream/:screamId', FBAuth, deleteScream);
+app.get('/scream/:screamId/like', FBAuth, likeScream);
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
+
+// User Routes
+
+app.post('/signup', signup);
+app.post('/login', login);
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
+
+// Notification System
+
+exports.createNotificationOnLike = functions.firestore
+  .document('likes/{id}')
+  .onCreate((snapshot) => {
+    db.doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'like',
+            read: false,
+            screamId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
       });
-      return res.json(screams);
-    })
-    .catch((err) => console.error(err));
-});
+  });
 
-// Post Screams
+exports.deleteNotificationOnUnLike = functions.firestore
+  .document('likes/{id}')
+  .onDelete((snapshot) => {
+    return db
+      .doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
 
-app.post('/scream', (req, res) => {
-  if (req.body.body.trim() === '') {
-    return res.status(400).json({ body: 'Body Must Not Be Empty' });
-  }
+exports.createNotificationOnComment = functions.firestore
+  .document('comments/{id}')
+  .onCreate((snapshot) => {
+    return db
+      .doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (
+          doc.exists &&
+          doc.data().userHandle !== snapshot.data().userHandle
+        ) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'comment',
+            read: false,
+            screamId: doc.id,
+          });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
 
-  const newScream = {
-    body: req.body.body,
-    userHandle: req.body.userHandle,
-    createdAt: new Date().toISOString(),
-  };
+exports.onUserImageChange = functions.firestore
+  .document('/users/{userId}')
+  .onUpdate((change) => {
+    console.log(change.before.data());
+    console.log(change.after.data());
+    if (change.before.data().imageUrl !== change.after.data().imageUrl) {
+      console.log('image has changed');
+      const batch = db.batch();
+      return db
+        .collection('screams')
+        .where('userHandle', '==', change.before.data().handle)
+        .get()
+        .then((data) => {
+          data.forEach((doc) => {
+            const scream = db.doc(`/screams/${doc.id}`);
+            batch.update(scream, { userImage: change.after.data().imageUrl });
+          });
+          return batch.commit();
+        });
+    } else return true;
+  });
 
-  db.collection('screams')
-    .add(newScream)
-    .then((doc) => {
-      res.json({ message: `document ${doc.id} created succesfully` });
-    })
-    .catch((err) => {
-      res.status(500).json({ error: `something went wrong` });
-      console.error(err);
-    });
-});
-
-const isEmail = (email) => {
-  const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  if (email.match(emailRegEx)) return true;
-  else return false;
-};
-
-const isEmpty = (string) => {
-  if (string.trim() === '') return true;
-  else return false;
-};
-
-// Signup route
-
-app.post('/signup', (req, res) => {
-  const newUser = {
-    email: req.body.email,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-    handle: req.body.handle,
-  };
-
-  let errors = {};
-
-  if (isEmpty(newUser.email)) {
-    errors.email = 'Must Not Be Empty';
-  } else if (!isEmail(newUser.email)) {
-    error.email = 'Must Be A Valid Email Address';
-  }
-
-  if (isEmpty(newUser.password)) errors.password = 'Must Not Be Empty';
-  if (newUser.password !== newUser.confirmPassword)
-    errors.confirmPassword = 'Password Must Match';
-  if (isEmpty(newUser.handle)) errors.handle = 'Must Not Be Empty';
-
-  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
-
-  //TODO validate data
-  let token, userId;
-
-  db.doc(`/users/${newUser.handle}`)
-    .get()
-    .then((doc) => {
-      if (doc.exists) {
-        return res.status(400).json({ handle: 'this handle is already taken' });
-      } else {
-        return firebase
-          .auth()
-          .createUserWithEmailAndPassword(newUser.email, newUser.password);
-      }
-    })
-    .then((data) => {
-      userId = data.user.uid;
-      return data.user.getIdToken();
-    })
-    .then((idToken) => {
-      token = idToken;
-      const userCredentials = {
-        handle: newUser.handle,
-        email: newUser.email,
-        createdAt: new Date().toISOString(),
-        userId,
-      };
-      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
-    })
-    .then(() => {
-      return res.status(201).json({ token });
-    })
-    .catch((err) => {
-      console.error(err);
-      if (err.code === 'auth/email-already-in-use') {
-        return res.status(400).json({ email: 'Email is already existed' });
-      } else {
-        return res.status(500).json({ error: err.code });
-      }
-    });
-});
-
-// Login Route
-
-app.post('/login', (req, res) => {
-  const user = {
-    email: req.body.email,
-    password: req.body.password,
-  };
-
-  let errors = {};
-
-  if (isEmpty(user.email)) errors.email = 'Must Not Be Empty';
-  if (isEmpty(user.password)) errors.password = 'Must Not Be Empty';
-
-  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
-
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(user.email, user.password)
-    .then((data) => {
-      return data.user.getIdToken();
-    })
-    .then((token) => {
-      return res.json({ token });
-    })
-    .catch((err) => {
-      console.error(err);
-      if (err.code === 'auth/wrong-password') {
-        return res
-          .status(403)
-          .json({ general: 'Wrong Credentials, Please Try Again' });
-      } else return res.status(500).json({ error: err.code });
-    });
-});
+exports.onScreamDelete = functions.firestore
+  .document('/screams/{screamId}')
+  .onDelete((snapshot, context) => {
+    const screamId = context.params.screamId;
+    const batch = db.batch();
+    return db
+      .collection('comments')
+      .where('screamId', '==', screamId)
+      .get()
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/comments/${doc.id}`));
+        });
+        return db.collection('likes').where('screamId', '==', screamId).get();
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/likes/${doc.id}`));
+        });
+        return db
+          .collection('notifications')
+          .where('screamId', '==', screamId)
+          .get();
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/notifications/${doc.id}`));
+        });
+        return batch.commit();
+      })
+      .catch((err) => console.error(err));
+  });
 
 exports.api = functions.https.onRequest(app);
